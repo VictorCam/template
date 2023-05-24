dba323c12ab510c6957b79c2f6c532e392af886e
VictorCam <43551031+VictorCam@users.noreply.github.com>
2023-05-23 19:31:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 import icons from "unplugin-icons/vite"
 import postcssPresetEnv from 'postcss-preset-env'
+import webfontDownload from 'vite-plugin-webfont-dl'
 
 export default defineConfig({
-  plugins: [sveltekit(), icons()],
+  plugins: [
+    sveltekit(),
+    icons(),
+    webfontDownload()
+  ],
   css: {
-		postcss: {
-			plugins: [postcssPresetEnv({stage: 2})]
-		}
-	},
+    postcss: {
+      plugins: [postcssPresetEnv({ stage: 2 })]
+    }
+  },
   server: {
     port: 2020,
   },
