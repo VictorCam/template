5eb637da077f931e259c306a99e331e3e5fb4c77
VictorCam <43551031+VictorCam@users.noreply.github.com>
2023-05-23 19:14:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import icons from "unplugin-icons/vite";
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
+import icons from "unplugin-icons/vite"
+import postcssPresetEnv from 'postcss-preset-env'
 
 export default defineConfig({
   plugins: [sveltekit(), icons()],
+  css: {
+		postcss: {
+			plugins: [postcssPresetEnv({stage: 2})]
+		}
+	},
   server: {
     port: 2020,
   },
