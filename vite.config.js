cf85774e1877bd8ff94836ec826f6a4b43c9e88a
VictorCam <campav@oregonstate.edu>
2023-05-31 21:09:37 -0500

    noramlize.css added / removed unplugin icons

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
-import icons from "unplugin-icons/vite"
 import postcssPresetEnv from 'postcss-preset-env'
 import webfontDownload from 'vite-plugin-webfont-dl'
 
 export default defineConfig({
   plugins: [
     sveltekit(),
-    icons(),
-    webfontDownload()
+    webfontDownload([])
   ],
   css: {
     postcss: {
