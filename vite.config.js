c36b6071e7a1bff9dd8c169841f1662f121e8ca9
Victor Campa <campav@oregonstate.edu>
2023-06-02 15:56:31 -0500

    reflected package changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
-import postcssPresetEnv from 'postcss-preset-env'
-import webfontDownload from 'vite-plugin-webfont-dl'
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
+import autoPrefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
-    webfontDownload([]) //https://www.cdnfonts.com/
+    webfontDownload([]), //https://www.cdnfonts.com/
   ],
   css: {
     postcss: {
-      plugins: [postcssPresetEnv({ stage: 2 })] //https://preset-env.cssdb.org/features/#stage-2
-    }
+      plugins: [autoPrefixer()],
+    },
   },
   server: {
     port: 2020,
-  }
-})
+  },
+});
