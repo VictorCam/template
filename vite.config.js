5133a7c351901034a3ec347ba605e20d6849a104
Victor Campa <campav@oregonstate.edu>
2023-06-08 07:51:06 -0500

    included ViteImageOptimizer

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import webfontDownload from "vite-plugin-webfont-dl";
 import autoPrefixer from "autoprefixer";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     webfontDownload([]), //https://www.cdnfonts.com/
+    ViteImageOptimizer({ png: {}, jpg: {}, jpeg: {}, webp: {}, svg: {} }),
   ],
   css: {
     postcss: {
