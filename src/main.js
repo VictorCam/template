fc83d0b6688f90378234b0facb0118a6a0f69177
Victor Campa <campav@oregonstate.edu>
2023-10-03 07:43:54 -0500

    404 and guards on navigation

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
 import Home from "./routes/Home.vue";
 import Page from "./routes/Page.vue";
+import NotFound from "./routes/404.vue";
+
+import cookie from "js-cookie";
 
 const routes = [
   {
@@ -20,12 +23,26 @@ const routes = [
     name: "page",
     component: Page,
   },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
 });
+
+// router.beforeEach((to, from, next) => {
+//   if (cookie.get("token")) next();
+//   if (!cookie.get("token")) {
+//     cookie.remove("token");
+//     next("/");
+//   }
+//   next();
+// });
 
 const pinia = createPinia();
 const app = createApp(App);
