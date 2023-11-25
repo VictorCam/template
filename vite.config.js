547b85bde9222e043e38ac46e4dba13d53d6796a
VictorCam <campav@oregonstate.edu>
2023-11-25 08:08:28 -0600

    updated config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,24 @@ import webfontDownload from "vite-plugin-webfont-dl";
 import autoPrefixer from "autoprefixer";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
+let font = 'https://fonts.cdnfonts.com'
+
 export default defineConfig({
   plugins: [
     sveltekit(),
-    webfontDownload([]), //https://www.cdnfonts.com/
-    ViteImageOptimizer({ png: {}, jpg: {}, jpeg: {}, webp: {}, svg: {} }),
+    webfontDownload([], { minifyCss: true, embedFonts: true }),
+    ViteImageOptimizer({
+      png: { quality: 40, effort: 9 },
+      jpg: { quality: 40, effort: 9 },
+      jpeg: { quality: 40, effort: 9 },
+      webp: { quality: 5, effort: 6 },
+      svg: {
+        js2svg: {
+          useShortTags: true,
+          pretty: false
+        }
+      }
+    }),
   ],
   css: {
     postcss: {
