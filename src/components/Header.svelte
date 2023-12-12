c58ddfbcbcafa35573ae3c60c6d5002edad886f2
Victor Campa <campav@oregonstate.edu>
2023-12-12 16:21:22 -0600

    default scaffold

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+    <div class="svg-color header-logo" />
+    <button
+        aria-label="Hamburger Menu"
+        >
+        <div clas="svg-color hamburger" /></button
+    >
+    <nav>
+        <ul>
+            <li><a href="#Link1">Link1</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    div {
+        margin-left: auto;
+    }
+
+
+</style>
---
+++ src/components/Modal.svelte
@@ -0,0 +1,29 @@
+<div class="modal hidden">
+    <div class="pos-modal">
+        <button
+            class="svg-button"
+            on:click|self={(e) => {
+                let modal = e.target.parentElement.parentElement;
+                modal.classList.add("hidden");
+            }}><div class="svg-color close" /></button
+        >
+        <div class="content">
+        </div>
+    </div>
+</div>
+
+<style>
+
+    .modal {
+        position: absolute;
+        display: grid;
+        z-index: 9999;
+        background: rgba(0, 0, 0, 0.8);
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
