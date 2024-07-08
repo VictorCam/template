9e04182e6e1be20601e0a187c1abc4e8a9670f07
Victor Campa <43551031+VictorCam@users.noreply.github.com>
2024-07-07 20:41:32 -0500

    work locale changes

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,9 +4,6 @@ import { useDark, useToggle, useIntersectionObserver, useScrollLock, useElementH
 import { useI18n } from 'vue-i18n';
 
 const { locale, availableLocales } = useI18n({ useScope: "global" });
-console.log(locale.value)
-console.log(availableLocales)
-console.log(usePreferredLanguages().value[0].split('-')[0])
 
 const languageCode = {
   'en': 'English',
@@ -22,6 +19,10 @@ let links = [
     name: "Page",
     href: "/page",
   },
+  {
+    name: "404",
+    href: "/404",
+  }
 ];
 
 let hamburgerElement = ref(null);
@@ -48,7 +49,12 @@ useIntersectionObserver(hamburgerElement, ([{ isIntersecting }]) => {
   isLocked.value = false
 })
 
-// console.log(locale)
+// set and get locale
+locale.value = localStorage.getItem('locale') || 'en'
+const setLocale = (lang) => {
+  locale.value = lang
+  localStorage.setItem('locale', lang)
+}
 
 const toggleSidebar = () => {
   toggleNavVisible()
@@ -63,11 +69,11 @@ const toggleSidebar = () => {
       <h1 class="mr-auto text-6">{{ $t("temp") }}</h1>
       <div class="relative">
         <button ref="LangElement" :aria-expanded="isLangHovered" class="svg-c <sm:hidden translate p3"></button>
-        <Transition name="fade">-
+        <Transition name="fade">
           <div v-if="isLangHovered || isDropdownHovered" ref="DropdownElement"
             class="b b-solid <sm:hidden absolute top-10 rd-3 flex gap2 p4 bg-light .dark:bg-dark">
             <ul class="flex gap2 flex-col">
-              <li v-for="lang in availableLocales" role="button" @click="locale = lang"
+              <li v-for="lang in availableLocales" role="button" @click="setLocale(lang)"
                 class="flex cursor-pointer gap1">{{ languageCode[lang] }} <span class="italic"
                   aria-hidden="true">▶</span></li>
             </ul>
@@ -84,14 +90,37 @@ const toggleSidebar = () => {
       <nav class="<sm:hidden p1">
         <ul class="flex flex-row gap3 m0">
           <li v-for=" link in links">
-            <router-link :to="link.href">
+            <router-link @click="toggleSidebar()" :to="link.href">
               <span>{{ link.name }}</span>
             </router-link>
           </li>
         </ul>
       </nav>
       <Transition name="fade">
-
+        <nav v-if="isNavVisible" ref="navElement"
+          class="sm:hidden bg-light .dark:bg-dark fixed z99 bottom-0 top-14 left-0 right-0 pt5">
+          <ul class="flex gap3 flex-col">
+            <li v-for="link in links">
+              <div class="grid justify-items-center m1">
+                <div class="flex gap2">
+                  <div class="svg-c tmp link-icons sm:hidden"></div>
+                  <router-link @click="toggleSidebar()" :to="link.href">
+                    <span>{{ link.name }}</span>
+                  </router-link>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div class=".dark:bg-gray-800 bg-gray-300 mx-auto w-fit flex flex-col gap5 mt-5 p5 rd-3">
+            <p class="flex gap2 c-gray-700 .dark:c-gray-500">Translations
+            <div class="svg-c translate"></div>
+            </p>
+            <div class="cursor-pointer" v-for="lang in availableLocales" role="button"
+              @click="setLocale(lang); toggleSidebar()">
+              <span>{{ languageCode[lang] }} <span class="italic" aria-hidden="true">▶</span></span>
+            </div>
+          </div>
+        </nav>
       </Transition>
     </div>
   </header>
