7edc58d1e508edcbee004284dcbed91d5f5b9573
VictorCam <vcampa@oregonstate.edu>
2024-10-12 22:28:27 -0500

    fixed date capture field

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -21,14 +21,16 @@ let { users, isLoadingUsers, errorUsers } = useUserStore()
 
     <!-- User Template -->
     <div v-if="isLoadingUsers" class="p3 flex flex-wrap gap3 mt3">
-      <div v-for="i in 10" class="animate-pulse bg-base-100 aspect-square w30 p2 rd-3"></div>
+      <div v-for="i in 12" class="animate-pulse bg-base-100 aspect-square w30 p2 rd-3"></div>
     </div>
     <!-- User Profiles -->
     <div class="p2 flex flex-wrap gap3 justify-start mt3" >
       <div class="rd-2 p2 flex flex-col justify-center bg-base-100" v-for="users in users.items" :id="users.id">
           <img @error="utils.handleImgError($event, placeHolder)" class="rd-2 aspect-square w30 rd-2" :src="utils.getImg(users.collectionId, users.id, users.avatar)" alt="test" />
           <p class="font-bold pb2">{{ users.username }}</p>
-          <p>Joined: {{ useDateFormat(users.createdAt, 'MMM DD YYYY') }}</p>
+          <!-- {{ users }} -->
+          <p>Joined: {{ useDateFormat(users.created, 'MMM DD YYYY') }}</p>
+          <!-- remove 5 characters at the end of string -->
       </div>
     </div>
     
